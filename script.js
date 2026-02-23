f03651524905c10b50df51a5098eeb0837add660
vinay3717 <vinaygole06@gmail.com>
2026-02-23 18:19:00 +0530

    Added Tables with use of float keyword

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-alert("JavaScript Connected!");
 let balance=0;
 function addTransaction(){
     let desc=document.getElementById("desc").value;
@@ -11,25 +10,47 @@ function addTransaction(){
     }
     amount=Number(amount);
 
-    if(type==="expense"){
-        balance=balance-amount;
-    }
-    else{
-        balance=balance+amount;
-    }
-    document.getElementById("balance").innerText=balance;
-    let li=document.createElement("li");
-    li.innerText=desc+"-Rs"+amount+" ("+type+") ";
-    
-    let deleteBtn=document.createElement("button");
-    deleteBtn.innerText="Delete";
+    let row = document.createElement("tr");
 
-    deleteBtn.onclick=function(){
-        li.remove();
+    let cell1 = document.createElement("td");
+    cell1.innerText = desc;
+
+    let cell2 = document.createElement("td");
+    cell2.innerText = "₹ " + amount;
+
+    let cell3 = document.createElement("td");
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Delete";
+
+    deleteBtn.onclick = function () {
+
+        if (type === "income") {
+            balance = balance - amount;
+        } else {
+            balance = balance + amount;
+        }
+
+        document.getElementById("balance").innerText = balance;
+
+        row.remove();
     };
-    li.appendChild(deleteBtn);
-    document.getElementById("list").appendChild(li);
 
-    document.getElementById("desc").value="";
-    document.getElementById("amount").value="";
+    cell3.appendChild(deleteBtn);
+
+    row.appendChild(cell1);
+    row.appendChild(cell2);
+    row.appendChild(cell3);
+
+    if (type === "income") {
+        document.getElementById("incomeTable").appendChild(row);
+        balance = balance + amount;
+    } else {
+        document.getElementById("expenseTable").appendChild(row);
+        balance = balance - amount;
+    }
+
+    document.getElementById("balance").innerText = balance;
+
+    document.getElementById("desc").value = "";
+    document.getElementById("amount").value = "";
 }
